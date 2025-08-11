ecaef1c70223d968e86ca8a1aa27c432c8876b80
Tayyab ali <2024cs491@student.uet.edu.pk>
2025-08-10 23:23:07 -0700

    .

--- Tasks/task11.js
+++ Tasks/task11.js
@@ -1,9 +1,12 @@
 //Write an example where hoisting creates unexpected behavior in a loop, then fix it.  
 for (var i = 1; i <= 3; i++) {
+        console.log("Loop is running, i =", i); // runs immediately
     setTimeout(function() {
         console.log("Number:", i); // Expect 1, 2, 3
     }, 1000);
 }
+console.log("Loop finished, i =", i); // runs right after loop
+
 for (let i = 1; i <= 3; i++) {
     setTimeout(function() {
         console.log("Number:", i);
@@ -13,7 +16,7 @@ for (var i = 1; i <= 3; i++) {
     (function(iCopy) {
         setTimeout(function() {
             console.log("Number:", iCopy);
-        }, 1000);
+        }, 1000); 
     })(i);
 } 
 
